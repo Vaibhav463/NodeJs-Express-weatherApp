02e479d6d86ecf340932760483dd13dd8e91565c
Vaibhav463 <63898687+Vaibhav463@users.noreply.github.com>
2021-09-26 21:27:31 +0530

    website hosting

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "c1124a7a94cda0c232e956cadefeeb9f"
+    }
+}
+
+module.exports = constants;
